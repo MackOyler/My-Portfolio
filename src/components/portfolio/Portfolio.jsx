93df31070fbb33f3d3b13ae61de2b2239eac56ca
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-05-07 18:10:53 -0500

    major portfolio updates

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,9 +4,9 @@ import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/codeQuiz.png'
 import IMG4 from '../../assets/pwGen.png'
-import IMG5 from '../../assets/HoriseonSEO.png'
-import IMG6 from '../../assets/portfolio6.jpg'
-import IMG7 from '../../assets/stlPets.jpg'
+import IMG5 from '../../assets/Brainwave.png'
+import IMG6 from '../../assets/HoriseonSEO.png'
+import IMG7 from '../../assets/2048.png'
 import IMG8 from '../../assets/portfolio5.png'
 
 // DO NOT USE THE IMAGES IN PRODUCTION
@@ -15,9 +15,9 @@ const data = [
   {
     id: 5,
     image: IMG5,
-    title: 'Horiseon: SEO',
-    github: 'https://github.com/MackOyler/Horiseon-SEO',
-    demo: 'https://mackoyler.github.io/Horiseon-SEO/'
+    title: 'Brainwave',
+    github: 'https://github.com/MackOyler/BrainWave',
+    demo: 'https://brainwavemack.com/'
   },
   {
     id: 2,
@@ -34,26 +34,26 @@ const data = [
     demo: 'https://github.com/MackOyler/SocialMediaAPI'
   },
   {
+    id: 1,
+    image: IMG7,
+    title: '2048',
+    github: 'https://github.com/MackOyler/2048',
+    demo: 'https://github.com/MackOyler/2048/commit/8f86463e6330299550c259392dbad0bcf670c5b8'
+  },
+  {
+    id: 5,
+    image: IMG6,
+    title: 'Horiseon: SEO',
+    github: 'https://github.com/MackOyler/Horiseon-SEO',
+    demo: 'https://mackoyler.github.io/Horiseon-SEO/'
+  },
+  {
     id: 4,
     image: IMG4,
     title: 'Password Generator ',
     github: 'https://github.com/MackOyler/RandomPasswordGenerator',
     demo: 'https://mackoyler.github.io/RandomPasswordGenerator/'
   },
-  {
-    id: 3,
-    image: IMG3,
-    title: 'Basic Code Quiz Game',
-    github: 'https://github.com/MackOyler/Code-Quiz',
-    demo: 'https://mackoyler.github.io/Code-Quiz/'
-  },
-  {
-    id: 1,
-    image: IMG7,
-    title: 'STL Pets',
-    github: 'https://github.com/MackOyler/Project-3-Repo',
-    demo: 'https://saint-louis-pets-554-d862dc3f7739.herokuapp.com/'
-  }
 ];
 
 
